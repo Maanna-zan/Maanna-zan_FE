7c41984a45c2bb268902bc0c309e703243055c3b
Pseheyon <boksil211@naver.com>
2023-04-10 03:28:49 +0900

    [feat] : seo및 헤더 전역 넣기, 게시글 조회 commentList,게시글  상세조회 넣어둠

--- src/pages/alcohols/index.js
+++ src/pages/alcohols/index.js
@@ -1,9 +1,10 @@
 import AlcoholList from '@features/AlcoholList';
 import React from 'react';
-
+import { HeadInfo } from '@components/Atoms/SEO/HeadInfo';
 const Alcohols = () => {
   return (
     <div>
+      <HeadInfo title="만나잔에 오신걸 환영합니다!" />
       <AlcoholList />
     </div>
   );
