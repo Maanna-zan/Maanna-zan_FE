973f250ee599f645697b4c9d41e4a2702321bbd5
최서윤 <cinnamon1995@naver.com>
2023-04-19 03:57:30 +0900

    로그인 모달 css 수정 및 정리

--- src/components/Modals/SignInModal.jsx
+++ src/components/Modals/SignInModal.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useMutation } from '@tanstack/react-query';
 import { apis } from '@shared/axios';
 import { cookies } from '@shared/cookie';
@@ -11,6 +11,7 @@ import { ButtonText } from '@components/Atoms/Button';
 export default function SignInModal({ onClose }) {
   const router = useRouter();
 
+  const [isEditMode, setIsEditMode] = useState('login');
   const [user, setUser] = React.useState({
     email: '',
     password: '',
@@ -51,77 +52,121 @@ export default function SignInModal({ onClose }) {
   });
 
   return (
-    <ModalDiv className="modal">
-      <div className="modal-overlay">
-        <InnerDiv>
-          <InputArea
-            className="InputArea"
-            type="text"
-            size="md"
-            name="email"
-            value={user.email}
-            onChange={changHandler}
-            placeholder="id를 입력하세요"
-          />
-          <InputArea
-            className="InputArea"
-            size="md"
-            type="password"
-            name="password"
-            value={user.password}
-            onChange={changHandler}
-            placeholder="비밀번호를 입력하세요"
-          />
-          <EttingDiv>
-            <div className="loginKeepGoing">
-              <input type="radio" />
-              <label htmlFor="name">로그인 유지</label>
-            </div>
-
-            <FindButton
-              onClick={() => {
-                router.push('/searchpassword');
-              }}
-            >
-              비밀번호 찾기
-            </FindButton>
-          </EttingDiv>
-
-          <ButtonText
-            style={{ marginTop: '30px' }}
-            label="로그인"
-            size="md"
-            variant="primary"
-            active={true}
-            onClick={() => {
-              register(user);
-            }}
-          />
-          <ButtonText
-            style={{ marginTop: '30px' }}
-            label="카카오로그인"
-            size="md"
-            variant="primary"
-            active={true}
-            onClick={() => {
-              kakao(user);
-            }}
-          />
-
-          <button
+    <>
+      <ModalDiv className="modal">
+        <div className="modal-overlay">
+          <img
             style={{
-              cursor: 'pointer',
-              border: 'none',
-              backgroundColor: 'transparent',
-              color: '#9fa4a9',
+              position: 'fixed',
+              right: '20px',
+              top: '20px',
+              // display: 'flex',
+              // justifyContent: 'flex-end',
+              width: '12px',
+              height: '12px',
             }}
             onClick={onClose}
-          >
-            Close
-          </button>
-        </InnerDiv>
-      </div>
-    </ModalDiv>
+            src="Group 1972.png"
+            alt="취소 버튼"
+          />
+          {isEditMode === 'login' ? (
+            <InnerDiv>
+              <h1 className="Login">로그인</h1>
+              <InputArea
+                className="InputArea"
+                type="text"
+                size="lg"
+                variant="default"
+                name="email"
+                value={user.email}
+                onChange={changHandler}
+                placeholder="id를 입력하세요"
+              />
+              <InputArea
+                className="InputArea"
+                size="lg"
+                variant="default"
+                type="password"
+                name="password"
+                value={user.password}
+                onChange={changHandler}
+                placeholder="비밀번호를 입력하세요"
+              />
+              <EttingDiv>
+                {/* <div className="loginKeepGoing">
+              <input name="name" type="radio" />
+              <label htmlFor="name">로그인 유지</label>
+            </div> */}
+
+                <FindButton
+                  onClick={() => {
+                    setIsEditMode('findPassword');
+                  }}
+                >
+                  비밀번호 찾기
+                </FindButton>
+              </EttingDiv>
+
+              <ButtonText
+                style={{ marginTop: '30px' }}
+                label="로그인"
+                size="md"
+                variant="primary"
+                active={true}
+                onClick={() => {
+                  register(user);
+                }}
+              />
+              <BottomDiv>
+                <p className="question"> 아이디가 없으신가요?</p>
+                <p className="goToSignUp" onClick={onClose}>
+                  회원가입
+                </p>
+              </BottomDiv>
+            </InnerDiv>
+          ) : (
+            <InnerDiv>
+              <h1 className="Login">비밀번호 찾기</h1>
+              <p className="notice">
+                가입 시 입력한 이메일 주소로 임시 비밀번호를 보내드립니다.
+              </p>
+              <p className="notice1">로그인 후 비밀번호를 변경해주세요.</p>
+              <InputArea
+                style={{ marginTop: '-15px' }}
+                type="text"
+                size="lg"
+                variant="default"
+                name="email"
+                value={user.email}
+                onChange={changHandler}
+                placeholder="이메일 주소를 입력하세요"
+              />
+              <ButtonText
+                style={{ marginTop: '30px' }}
+                label="임시 비밀번호 전송"
+                size="md"
+                variant="primary"
+                active={true}
+                onClick={() => {
+                  register(user);
+                }}
+              />
+              <BottomDiv style={{ marginTop: '100px' }}>
+                <p className="question"> 비밀번호가 기억났어요!</p>
+                <p
+                  className="goToSignUp"
+                  onClick={() => {
+                    setIsEditMode('login');
+                  }}
+                >
+                  로그인
+                </p>
+              </BottomDiv>
+            </InnerDiv>
+          )}
+        </div>
+      </ModalDiv>
+    </>
   );
 }
 
@@ -145,7 +190,7 @@ const ModalDiv = styled.div`
     justify-content: center;
     background-color: rgba(255, 255, 255);
     z-index: 1000;
-    max-width: 400px;
+    max-width: 410px;
     min-width: 280px;
     width: 50%;
     border: 1px solid #939aa0;
@@ -158,18 +203,43 @@ const InnerDiv = styled.div`
   flex-direction: column;
   justify-content: center;
   align-content: center;
-  row-gap: 10px;
+  row-gap: 20px;
   .InputArea {
-    border-color: #9fa4a9;
     width: 250px;
     display: flex;
-    justify-items: center;
+    justify-content: center;
+  }
+  .Login {
+    margin-top: -2px;
+    font-weight: 600;
+    font-size: 20px;
+    line-height: 26px;
+    display: flex;
+    justify-content: center;
+  }
+  .notice {
+    margin-top: -30px;
+    display: flex;
+    justify-content: center;
+    font-weight: 400;
+    font-size: 12px;
+    line-height: 16px;
+    color: #9fa4a9;
+  }
+  .notice1 {
+    margin-top: -28px;
+    display: flex;
+    justify-content: center;
+    font-weight: 400;
+    font-size: 12px;
+    line-height: 16px;
+    color: #9fa4a9;
   }
 `;
 
 const EttingDiv = styled.div`
   display: flex;
-  justify-content: space-between;
+  justify-content: flex-end;
   align-items: center;
   .loginKeepGoing {
     color: #9fa4a9;
@@ -185,4 +255,19 @@ const FindButton = styled.button`
   background-color: transparent;
   width: fit-content;
   font-size: 11px;
+`;
+
+const BottomDiv = styled.div`
+  margin-top: 20px;
+  display: flex;
+  justify-content: space-evenly;
+  font-weight: 700;
+  font-size: 12px;
+  line-height: 16px;
+  .question {
+    color: #9fa4a9;
+  }
+  .goToSignUp {
+    color: #ff6a64;
+  }
 `;
