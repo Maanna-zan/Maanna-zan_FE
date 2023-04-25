9f6c8681232d51b940ad11a7cafb4b26cc2c0821
Jay, Jaehyeok Yun <124968754+Mindfulness-en-toute-Tranquillite@users.noreply.github.com>
2023-04-26 00:31:03 +0900

    [feat]mapmidpoint 페이지 생성,  MapMidPoint.js features파일 생성 및 기본 세팅

---
+++ src/pages/mapmidpoint/index.js
@@ -0,0 +1,12 @@
+import MapMidPoint from '@features/map/MapMidPoint'
+import React from 'react'
+
+function index() {
+    return (
+        <>
+            <MapMidPoint/>
+        </>
+    )
+}
+
+export default index
