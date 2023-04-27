fb4912c1f115a6d057cfa50cf31a8ae59210019f
Jay, Jaehyeok Yun <124968754+Mindfulness-en-toute-Tranquillite@users.noreply.github.com>
2023-04-27 22:34:27 +0900

    [fix]contextAPI로 props -> react-query로 props전달. 페이지 이동 후에도 props값 휘발되지 않음.

--- src/features/map/MapMidPoint.js
+++ src/features/map/MapMidPoint.js
@@ -1,35 +1,38 @@
 import { FlexRow } from '@components/Atoms/Flex'
 import { WebWrapper, WebWrapperHeight } from '@components/Atoms/Wrapper'
-import { MidPointContext } from '@components/Modals/SearchKeywordModalPortal';
+import { MidPointContext, PassPropsWithContextAPI } from '@components/Modals/SearchKeywordModalPortal';
 import { apis } from '@shared/axios';
-import { useQuery } from '@tanstack/react-query';
+import { useQuery, useQueryClient } from '@tanstack/react-query';
 import { useRouter } from 'next/router';
 import React, { useContext, useEffect, useState } from 'react'
 
 function MapMidPoint() {
-    // const serverUrl = `${process.env.NEXT_PUBLIC_SERVER_KEY}/kakaoApi?query=술집&radius=1500&page=1&size=15`
+    //  MapMidPoint 컴포넌트에 보여지는 요소 안 보이게 하기
+    const [showVue, setShowVue] = useState(false)
+    // console.log("midPoint===>",midPoint);
     // useContext Hook을 사용하여 MidPointContext 컨텍스트 값을 가져온다. (midPoint 값)
-    const midPoint = useContext(MidPointContext);
-    console.log(midPoint);
-    console.log(MidPointContext);
-    const [midPointProp, setMidPointProp] = useState();
-    //  중간 위치 탐색 page 이동 위한 useRouter선언.
-    const router = useRouter();
-    // //  중간 위치 탐색 페이지 이동 버튼 핸들러
-    // const moveToMapMidPointButtonClickHandler = () => {
-    //     router.push('/mapmidpoint');
-    // };
-    const { data, isLoading, isError, refetch } = useQuery({
+    // const midPoint = useContext(MidPointContext);
+    // console.log("midPoint===>",midPoint);
+
+    // midPoint값 props로 받아 state로 저장.
+    // const [midPointProp, setMidPointProp] = useState();
+
+
+    // 서버와 통신
+        const { data, isLoading, isError, refetch } = useQuery({
         queryKey: ['GET_KAKAOAPI'],
         queryFn: async () => {
+        console.log("response",response)
         const response = await apis.get(
             // 서버 URL
-            `/kakaoApi?y=${midPoint.lat}&x=%20${midPoint.lng}&query=술집&radius=1500&page=1&size=15&sort=distance`,
+            `/kakaoApi?y=${midPoint?.lat}&x=%20${midPoint?.lng}&query=술집&radius=1500&page=1&size=15&sort=distance`,
             // 중간지점 lat,lng값
-            midPoint
+            midPointProp
         );
-        return response.data.message;
-    }}, 
+        return response;
+        },
+        staleTime: 6000000 // 100분 (단위: 밀리초)
+    }, 
     {
         // 에러 처리
         onError: (error) => {
@@ -40,20 +43,26 @@ function MapMidPoint() {
             alert(data);
         }
     });
-    const handleClick = () => {
-        refetch()
-        router.push('/mapmidpoint');
-    };
+    // console.log("midPoint===>",midPoint);
+
+
+    // const queryClient = useQueryClient();
+    // // const midPointProp = queryClient.getQueryData('midPointProp');
+    // const { data: midPointProp } = useQuery('midPointProp', () => {
+    //     return fetch('http://http://localhost:3000/map').then((res) => res.json());
+    //   });
+
+    const queryClient = useQueryClient();
+    const midPointProp = queryClient.getQueryData({queryKey: ['FUCK']});
+    console.log("ㅗㅗㅗmidPointPropㅗㅗㅗ",midPointProp)
+    
 
     return (
         <WebWrapper>
             <WebWrapperHeight>
                 <FlexRow style={{ justifyContent: 'space-between' }}>
-                    <button onClick={handleClick}>
-                        검색
-                    </button>
-                    <div></div>
-                    <input></input>
+                {showVue && <button onClick={handleClick}>버튼</button>}
+                {/* <div>{midPointProp}</div> */}
                 </FlexRow>
             </WebWrapperHeight>
         </WebWrapper>
@@ -61,3 +70,41 @@ function MapMidPoint() {
 }
 
 export default MapMidPoint
+
+
+
+
+
+
+
+
+
+
+    // const { data, isLoading, isError, refetch } = useQuery({
+    //     queryKey: ['GET_KAKAOAPI'],
+    //     queryFn: async () => {
+    //     console.log("response",response)
+    //     const response = await apis.get(
+    //         // 서버 URL
+    //         `/kakaoApi?y=${midPoint?.lat}&x=%20${midPoint?.lng}&query=술집&radius=1500&page=1&size=15&sort=distance`,
+    //         // 중간지점 lat,lng값
+    //         midPoint
+    //     );
+    //     return response;
+    //     },
+    //     staleTime: 6000000 // 100분 (단위: 밀리초)
+    // }, 
+    // {
+    //     // 에러 처리
+    //     onError: (error) => {
+    //         console.error(error);
+    //     },
+    //     // 패칭 데이터 상태 변화
+    //     onSettled: (data) => {
+    //         alert(data);
+    //     }
+    // });
+    // useEffect(() => {
+    //     refetch();
+    // }, [router]);
+    // //
