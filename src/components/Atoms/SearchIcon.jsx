48113a24803224464ee6d6bdaa6cb0f771d024ed
Pseheyon <100889975+Pseheyon@users.noreply.github.com>
2023-05-01 21:43:05 +0900

    [fix]메인 라우팅, 스토어 상세 수정,중간 풀푸쉬를 위한 커밋

--- src/components/Atoms/SearchIcon.jsx
+++ src/components/Atoms/SearchIcon.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 export const SearchIcon = () => {
   return (
     <svg
+      style={{ cursor: 'pointer' }}
       width="24"
       height="24"
       viewBox="0 0 24 24"
